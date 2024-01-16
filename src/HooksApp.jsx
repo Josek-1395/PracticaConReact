8a02f2a4eaf7d55eff43ed9b01872fe637eb3050
Jose <jose.khairallah.utn@gmail.com>
2024-01-16 00:19:51 -0300

    funcionando con API de JSON placeholder

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -1,10 +1,10 @@
-import { FormularioComponent } from "./components/FormularioComponent"
+import { UsuariosComponent } from "./components/UsuariosComponent"
 export const HooksApp = () => {
   return (
     <>
     <h1>Aplicacion de Hooks</h1>
     <hr />
-    <FormularioComponent></FormularioComponent>
+    <UsuariosComponent></UsuariosComponent>
     </>
   )
 }
