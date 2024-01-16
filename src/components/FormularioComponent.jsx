aa9062eb093b50a42dacba56b4a48b21979ea924
Jose <jose.khairallah.utn@gmail.com>
2024-01-15 23:42:03 -0300

    funcionando con Custom Hook separado de Componente

--- src/components/FormularioComponent.jsx
+++ src/components/FormularioComponent.jsx
@@ -1,22 +1,14 @@
 
 import { useState } from "react"
+import { useForm } from "../hooks/useForm"
 export const FormularioComponent = () => {
-
-    const [formState, setFormState] = useState({
+    const initialForm = {
         userName: '',
         email: '',
         password: ''
-    })
-    const {userName, email, password} = formState
-
-    const onInputChange = ({target}) => {
-        const {id, value} = target
-        setFormState({
-            ...formState,
-            [id] : value
-        })
-
     }
+    const { formState, onInputChange } = useForm(initialForm)
+    const { userName, email, password } = formState
     const onSubmit = (evento) => {
         evento.preventDefault()
         console.log(formState);
@@ -30,31 +22,31 @@ export const FormularioComponent = () => {
                     className="form-control"
                     id="userName"
                     placeholder="Enter User Name"
-                    value= {userName}
-                    onChange={onInputChange}  />
+                    value={userName}
+                    onChange={onInputChange} />
 
             </div>
             <div className="form-group">
                 <label htmlFor="email">Email address</label>
                 <input
-                    
+
                     className="form-control"
                     id="email"
                     placeholder="Enter email"
-                    value= {email}
-                    onChange={onInputChange}  />
+                    value={email}
+                    onChange={onInputChange} />
 
             </div>
 
             <div className="form-group">
                 <label htmlFor="password">Password</label>
                 <input
-                    
+
                     className="form-control"
                     id="password"
                     placeholder="Password"
-                    value= {password}
-                    onChange={onInputChange}  />
+                    value={password}
+                    onChange={onInputChange} />
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
